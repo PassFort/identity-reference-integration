86ace7c46e788def2aaf3a4ad122156da7bb538c
Diggory Blake <diggsey@googlemail.com>
2020-01-06 14:07:10 +0000

    Add reference integration

---
+++ app/application.py
@@ -0,0 +1,14 @@
+import os
+
+from flask import Flask
+
+
+# If `entrypoint` is not defined in app.yaml, App Engine will look for an app
+# called `app` in `main.py`.
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    """Return a friendly HTTP greeting."""
+    return 'Hello World!'
